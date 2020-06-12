b5ebe9718bf8e126994b2dc293435a270e22b5cb
Ashton Threadwell <kanyesouthwest@users.noreply.github.com>
2020-06-12 12:23:24 +1200

    PHP Updates

---
+++ Student/search.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+
+    <h1>Search for a student by first name</h1>
+    <p>Enter any part of the student's name e.g. John or J</p>
+    <form class="" action = "result.php" method = "post">
+      Name : <input type="text" name="name_entered" value="">
+      <button type="submit" name="button">Go!</button>
+  </body>
+</html>
